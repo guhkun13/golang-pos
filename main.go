0c54250b214247713ed217a4043f1a7db0b8546f
guhkun13 <guhkun13@gmail.com>
2022-05-09 01:20:53 +0800

    refactor agains

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/qinains/fastergoding"
 
-	"github.com/guhkun13/go-pos/apps/router"
+	"github.com/guhkun13/go-pos/apps/routes"
 	"github.com/guhkun13/go-pos/pkg/database"
 	"github.com/guhkun13/go-pos/pkg/utils"
 )
 
 func main() {
-
 	log.Println("Start server")
 	
 	// auto reload
@@ -25,7 +24,7 @@ func main() {
 	database.PostgresConnection()
 
 	// setup routes
-	router.SetupRoutes(app)
+	routes.SetupRoutes(app)
 
 	// start server
 	utils.StartServer(app)
--- pkg/database/postgres_connection.go
+++ pkg/database/postgres_connection.go
@@ -6,26 +6,32 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
-	"github.com/guhkun13/go-pos/apps/user"
+	user_model "github.com/guhkun13/go-pos/apps/user/model"
+	// product_model "github.com/guhkun13/go-pos/apps/product/model"
 	"github.com/guhkun13/go-pos/pkg/utils"
 )
 
+var DB *gorm.DB
+
 func PostgresConnection() {
+	
 	// import "gorm.io/driver/postgres"
 	// ref: https://gorm.io/docs/connecting_to_the_database.html#PostgreSQL
 	dsn, _ := utils.ConnectionURLBuilder("postgres")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
 	if err != nil {
 		panic("Failed connect to database")
 	}
-
+	
 	log.Println("Connection opened to database")
-
+	
 	// Migrate DB
-	db.AutoMigrate(&user.UserModel{})
+	tables := []string{"User", "Product"}
 
-	log.Println("Database migrated")
-
+	DB.AutoMigrate(&user_model.User{})
+	// DB.AutoMigrate(&product_model.Product{})
+	
+	log.Println("Database migrated : ", tables)
 	
 }
