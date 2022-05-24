9ac8392f4d96544836b40de88dfad848c8259646
guhkun13 <guhkun13@gmail.com>
2022-05-24 14:11:26 +0800

    implement register and login. Verify input before processing

--- routes/auth.go
+++ routes/auth.go
@@ -4,9 +4,11 @@ import (
 	"log"
 	"net/http"
 	"time"
+	// "time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	// "github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 
@@ -22,7 +24,7 @@ type RegisterUserDto struct {
 
 
 type UserLoginDto struct {
-	Email string
+	Email string			
 	Password string
 }
 
@@ -35,11 +37,12 @@ func Register(c *fiber.Ctx) error {
 		return err
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data.Password), 12)
-
-	if data.Name == "" || data.Email == "" {
-		return c.Status(http.StatusBadRequest).JSON("Fix your data payload")
+	if data.Name == "" || data.Email == "" || data.Password == "" {
+		msg := fiber.Map{"status": false, "message":"Fix your data payload"}
+		return c.Status(http.StatusBadRequest).JSON(msg)
 	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data.Password), 12)
 
 	user := models.User{
 		ID: 			uuid.New(),
@@ -52,30 +55,51 @@ func Register(c *fiber.Ctx) error {
 		return c.Status(http.StatusInternalServerError).JSON(err.Error())
 	}
 
-	return c.Status(http.StatusOK).JSON(user)
+	return c.Status(http.StatusOK).JSON("successfully create register new user")
 }
 
 func Login(c *fiber.Ctx) error {
 
 	log.Println("method login!")
-	var account UserLoginDto
-	// user := c.FormValue("user")
-	// pass := c.FormValue("pass")
+	var data UserLoginDto
 
-	if err := c.BodyParser(&account); err != nil {
+	if err := c.BodyParser(&data); err != nil {
 		log.Println("Error on parsing" ,err.Error())
 	}
 
-	log.Println("success parsing body:", account)
+	if data.Email == "" || data.Password == "" {
+		msg := fiber.Map{"status": false, "message":"Fix your data payload"}
+		return c.Status(http.StatusBadRequest).JSON(msg)
+	}
 
-	// Throws Unauthorized error
-	// if account.u != "john" || pass != "doe" {
-	// 	return c.SendStatus(fiber.StatusUnauthorized)
-	// }
+	log.Println("success parsing body:", data)
+
+	var user models.User
+	if err := database.DbSql.Find(&user, "email = ?", data.Email).Error; err != nil {
+		log.Println("Error finding data", err.Error())
+	}
+	
+	// log.Println(uuid.)
+	if user.ID == uuid.Nil {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	log.Println("data found = ", user)
+
+	log.Printf("Compare password input vs db =  \n %s \n %s \n ", []byte(data.Password), []byte(user.Password))
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data.Password)); err != nil {
+		log.Println("Error on comparing password : ", err.Error())
+		c.Status(fiber.StatusUnauthorized)
+
+		return c.JSON(fiber.Map{
+			"message": "Invalid Credentials",
+		})
+	}
 
 	// Create the Claims
 	claims := jwt.MapClaims{
-		"name":  "John Doe",
+		"name":  user.Name,
 		"admin": true,
 		"exp":   time.Now().Add(time.Hour * 72).Unix(),
 	}
@@ -84,7 +108,7 @@ func Login(c *fiber.Ctx) error {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	// Generate encoded token and send it as response.
-	t, err := token.SignedString([]byte("secret"))
+	t, err := token.SignedString([]byte(SecretKey))
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
